04ae982c60250ec869e6710aa6955a140ff31ae4
zhlongfj <zhlongfj@sina.com>
2015-02-12 10:31:11 +0800

    test fighting when run 3 rounds

--- java/maven/src/main/java/player/Player.java
+++ java/maven/src/main/java/player/Player.java
@@ -1,14 +1,18 @@
 package player;
 
+import java.io.PrintStream;
+
 /**
  * Created by zhl on 15/2/12.
  */
 public class Player {
+    private PrintStream out;
     private String name;
     private int healthPoint;
     private int attackPoint;
 
-    public Player(String name, int healthPoint, int attackPoint) {
+    public Player(PrintStream out, String name, int healthPoint, int attackPoint) {
+        this.out = out;
 
         this.name = name;
         this.healthPoint = healthPoint;
@@ -19,12 +23,19 @@ public class Player {
         return name;
     }
 
+    public int getHealthPoint() {
+        return healthPoint;
+    }
+
     public boolean canAttack() {
         return healthPoint > 0 ? true : false;
     }
 
     public void attack(Player player2) {
         player2.reduceHealthPoint(attackPoint);
+        out.println(name + "攻击了" + player2.getName() + ","
+                + player2.getName() + "受到了" + attackPoint + "点伤害,"
+                + player2.getName() + "剩余生命：" + player2.getHealthPoint());
     }
 
     public void reduceHealthPoint(int attackedPoint) {
--- java/maven/src/test/java/GameProcessorTest.java
+++ java/maven/src/test/java/GameProcessorTest.java
@@ -1,10 +1,12 @@
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InOrder;
 import player.Player;
 
 import java.io.PrintStream;
 
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
@@ -23,8 +25,8 @@ public class GameProcessorTest {
 
     @Test
     public void should_print_lisi_is_defeated() {
-        Player player1 = new Player("张三", 20, 8);
-        Player player2 = new Player("李四", 10, 9);
+        Player player1 = new Player(out, "张三", 20, 8);
+        Player player2 = new Player(out, "李四", 10, 9);
         game = new GameProcessor(out, player1, player2);
 
         game.start();
@@ -44,4 +46,33 @@ public class GameProcessorTest {
 
         verify(out).println("张三被打败了");
     }
+
+    @Test
+    public void should_print_zhangsan_is_defeated_when_run_3_rounds() {
+        Player player1 = mock(Player.class);
+        Player player2 = mock(Player.class);
+        game = new GameProcessor(out, player1, player2);
+        given(player1.getName()).willReturn("张三");
+        given(player1.canAttack()).willReturn(true, true, false);
+        given(player2.canAttack()).willReturn(true);
+        game.start();
+
+        verify(out).println("张三被打败了");
+    }
+
+    @Test
+    public void should_print_lisi_is_defeated_when_run_3_rounds() {
+        Player player1 = mock(Player.class);
+        Player player2 = mock(Player.class);
+        game = new GameProcessor(out, player1, player2);
+        given(player2.getName()).willReturn("李四");
+        given(player1.canAttack()).willReturn(true);
+        given(player2.canAttack()).willReturn(true, true, false);
+        game.start();
+
+        verify(out).println("李四被打败了");
+    }
+
+
+
 }
